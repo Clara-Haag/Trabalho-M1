bacb2bb4ea16a9116d76b6214c07feaaa568ae1a
Clara Haag <cla.hrd@gmail.com>
2025-03-30 11:01:18 -0300

    ADD cálculo do segundo dígito | UPDATE de README.md | REMOVE teste.cpp

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Criação de verificação de CPF
 ## Pontos já feitos
 - [X] Verificação de tamanho de Input
 - [X] Cálculo do primeiro dígito verificador
-- [ ] Cálculo do segundo dígito verificador
+- [X] Cálculo do segundo dígito verificador
+- [ ] Documentar código
 # Informações importantes
 - Data de entrega: 08/04/25
 - Não pode utilizar funções e arrays
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-  int cpf, d1, d2, d3, d4, d5, d6, d7, d8, d9; // inicia as variáveis do CPF e dos dígitos de 1 a 9 (D1..D9)
+  int cpf, d1, d2, d3, d4, d5, d6, d7, d8, d9; // Inicia as variáveis do CPF e dos dígitos de 1 a 9 (D1..D9)
 
   cout << "Informe um número de 9 dígitos: ";
   cin >> cpf;
@@ -29,9 +29,9 @@ int main() {
   r7 = d7 * 4;
   r8 = d8 * 3;
   r9 = d9 * 2;
-  int r_soma = r1+ r2+ r3 + r4 + r5+ r6 + r7 + r8 + r9; // soma os resultados anteriores
+  int r_soma = r1+ r2+ r3 + r4 + r5+ r6 + r7 + r8 + r9; // Soma os resultados anteriores
 
-  int d_verificador_1 = r_soma % 11;
+  int d_verificador_1 = r_soma % 11; // Caso o resultado seja igual ou menor a 2, o dígito será igual a 0!
   if (d_verificador_1 <= 2){
     d_verificador_1 = 0;
   } else {
@@ -39,4 +39,31 @@ int main() {
   }
 
   cout << "Dígito verificador 1: " << d_verificador_1 << endl; // endl é o comando para dar quebra de linha!
+
+  // Cálculo do segundo dígito verificador: reutilizando váriaveis anteriores (R1..R9)
+  int r10;
+  r1 = d1 * 11;
+  r2 = d2 * 10;
+  r3 = d3 * 9;
+  r4 = d4 * 8;
+  r5 = d5 * 7;
+  r6 = d6 * 6;
+  r7 = d7 * 5;
+  r8 = d8 * 4;
+  r9 = d9 * 3;
+  r10 = d_verificador_1 * 2;
+
+  r_soma = r1+ r2+ r3 + r4 + r5+ r6 + r7 + r8 + r9 + r10; // Soma os resultados anteriores
+
+  int d_verificador_2 = r_soma % 11; // Caso o resultado seja igual ou menor a 2, o dígito será igual a 0!
+  if (d_verificador_2 <= 2){
+    d_verificador_2 = 0;
+  } else {
+      d_verificador_2 = 11 - d_verificador_2;
+  }
+
+  cout << "Dígito verificador 2: " << d_verificador_2 << endl;
+
+  // Formata o CPF (xxx.xxx.xxx-xx) e mostra ao usuário
+  cout << "CPF completo: " << d1 << d2 << d3 << "." << d4 << d5 << d6 << "." << d7 << d8 << d9 << "-" << d_verificador_1 << d_verificador_2 << endl;
 }
