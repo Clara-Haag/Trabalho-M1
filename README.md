b676e2f61821878aba039ddb1b5cca8c86aae7d8
Clara Haag <cla.hrd@gmail.com>
2025-04-08 23:41:36 -0300

    UPDATE: algoritmo de cálculo de dígitos

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Trabalho M1
 Criação de verificação de CPF
 ## Pontos já feitos
-- [X] Verificação de tamanho de Input
+- [ ] Verificação de tamanho de Input
 - [X] Cálculo do primeiro dígito verificador
 - [X] Cálculo do segundo dígito verificador
-- [ ] Documentar código
+- [X] Documentar código
 # Informações importantes
 - Data de entrega: 08/04/25
 - Não pode utilizar funções e arrays
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-  int cpf, d1, d2, d3, d4, d5, d6, d7, d8, d9; // Inicia as variáveis do CPF e dos dígitos de 1 a 9 (D1..D9)
+  int cpf, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11; // Inicia as variáveis do CPF e dos dígitos de 1 a 9 (D1..D9) e os dos dígitos verificadores (d10 e d11)
 
   cout << "Informe um número de 9 dígitos: ";
   cin >> cpf;
@@ -19,51 +19,21 @@ int main() {
   d9 = cpf % 10;
 
   // Calculando o primeiro dígito verificador:
-  int r1, r2, r3, r4, r5, r6, r7, r8, r9; // Inicia as váriaveis de resultado da múltiplicação dos dígitos
-  r1 = d1 * 10;
-  r2 = d2 * 9;
-  r3 = d3 * 8;
-  r4 = d4 * 7;
-  r5 = d5 * 6;
-  r6 = d6 * 5;
-  r7 = d7 * 4;
-  r8 = d8 * 3;
-  r9 = d9 * 2;
-  int r_soma = r1+ r2+ r3 + r4 + r5+ r6 + r7 + r8 + r9; // Soma os resultados anteriores
+  d10 = (d1 * 10 + d2 * 9 + d3 * 8 + d4 * 7 + d5 * 6 + d6 * 5 + d7 * 4 + d8 * 3 + d9 * 2)%11;
 
-  int d_verificador_1 = r_soma % 11; // Caso o resultado seja igual ou menor a 2, o dígito será igual a 0!
-  if (d_verificador_1 <= 2){
-    d_verificador_1 = 0;
-  } else {
-      d_verificador_1 = 11 - d_verificador_1;
-  }
+  if (d10 < 2){ // Se for 1 ou 0, ele é igual a 0
+    d10 = 0;
+    } else { d10 = 11 - d10; // Caso for 2 ou mais, subtrai de 11
+    }
 
-  cout << "Dígito verificador 1: " << d_verificador_1 << endl; // endl é o comando para dar quebra de linha!
+  // Calculando o segundo dígito verificador
+  d11 = (d1 * 11 + d2 * 10 + d3 * 9 + d4 * 8 + d5 * 7 + d6 * 6 + d7 * 5 + d8 * 4 + d9 * 3 + d10 * 2)%11;
 
-  // Cálculo do segundo dígito verificador: reutilizando váriaveis anteriores (R1..R9)
-  int r10;
-  r1 = d1 * 11;
-  r2 = d2 * 10;
-  r3 = d3 * 9;
-  r4 = d4 * 8;
-  r5 = d5 * 7;
-  r6 = d6 * 6;
-  r7 = d7 * 5;
-  r8 = d8 * 4;
-  r9 = d9 * 3;
-  r10 = d_verificador_1 * 2;
-
-  r_soma = r1+ r2+ r3 + r4 + r5+ r6 + r7 + r8 + r9 + r10; // Soma os resultados anteriores
-
-  int d_verificador_2 = r_soma % 11; // Caso o resultado seja igual ou menor a 2, o dígito será igual a 0!
-  if (d_verificador_2 <= 2){
-    d_verificador_2 = 0;
-  } else {
-      d_verificador_2 = 11 - d_verificador_2;
-  }
-
-  cout << "Dígito verificador 2: " << d_verificador_2 << endl;
+  if (d11 < 2){ // Se for 1 ou 0, ele é igual a 0
+    d11 = 0;
+    } else { d11 = 11 - d11; // Caso for 2 ou mais, subtrai de 11
+    }
 
   // Formata o CPF (xxx.xxx.xxx-xx) e mostra ao usuário
-  cout << "CPF completo: " << d1 << d2 << d3 << "." << d4 << d5 << d6 << "." << d7 << d8 << d9 << "-" << d_verificador_1 << d_verificador_2 << endl;
+  cout << "CPF completo: " << d1 << d2 << d3 << "." << d4 << d5 << d6 << "." << d7 << d8 << d9 << "-" << d10 << d11 << endl;
 }
